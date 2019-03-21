be06d6dbaaddcfb50abd26ad3d6239bcd0143f14
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-03-21 13:35:28 -0700

    Add mock data

--- app.jsx
+++ app.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
+const initialState = {
+  1553202000: {
+    status: 'Clear sky',
+    high: 15,
+    low: 14,
+    humidity: 82,
+    wind: 3,
+    rain: 0,
+  },
+  1553212800: {
+    status: 'Clear sky',
+    high: 14,
+    low: 13,
+    humidity: 81,
+    wind: 4,
+    rain: 0,
+  },
+  1553223600: {
+    status: 'Clear sky',
+    high: 10,
+    low: 11,
+    humidity: 89,
+    wind: 2,
+    rain: 0,
+  },
+};
+
 const App = () => (
   <div>Hello from React!!!</div>
 );
